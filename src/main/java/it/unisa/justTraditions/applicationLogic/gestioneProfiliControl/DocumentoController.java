cb33ed0de1baf4683b885c1da67d562ca055648d
Giovanni Miele <106318586+GiovanniMiele18@users.noreply.github.com>
2025-10-24 03:13:09 +0200

    Update DocumentoController.java

--- src/main/java/it/unisa/justTraditions/applicationLogic/gestioneProfiliControl/DocumentoController.java
+++ src/main/java/it/unisa/justTraditions/applicationLogic/gestioneProfiliControl/DocumentoController.java
@@ -19,7 +19,11 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.io.IOException;
 
 @Controller
